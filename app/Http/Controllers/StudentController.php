ea52841fad416bc7f38c0bcfbf69dcd0af08c471
ArthurPerozin <135287664+ArthurPerozin@users.noreply.github.com>
2024-03-17 21:23:13 -0300

    Refactor: update search method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,14 +10,22 @@ class StudentController extends Controller
     public function index(Request $request)
     {
         try {
-            $searchQuery = $request->input('q');
+            $name = $request->input('name');
+            $cpf = $request->input('cpf');
+            $email = $request->input('email');
 
             $studentsQuery = Student::query();
 
-            if ($searchQuery) {
-                $studentsQuery->where('name', 'like', "%$searchQuery%")
-                              ->orWhere('cpf', 'like', "%$searchQuery%")
-                              ->orWhere('email', 'like', "%$searchQuery%");
+            if ($name) {
+                $studentsQuery->where('name', 'like', "%$name%");
+            }
+
+            if ($cpf) {
+                $studentsQuery->orWhere('cpf', 'like', "%$cpf%");
+            }
+
+            if ($email) {
+                $studentsQuery->orWhere('email', 'like', "%$email%");
             }
 
             $students = $studentsQuery->orderBy('name')->get();
