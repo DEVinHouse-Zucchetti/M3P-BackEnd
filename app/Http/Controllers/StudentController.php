de36802954da44be4b85e13a74524301a1dd7f69
ArthurPerozin <135287664+ArthurPerozin@users.noreply.github.com>
2024-03-21 21:08:00 -0300

    Update: StudenController
    - User can only list stundets wich belongs to User
    - Filter without params
    - Filter by name
    - Filter by email
    - Filter by cpf
    
    - Response with student id, name, email, contact and cpf

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,28 +1,58 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use App\Models\Student;
 use App\Traits\HttpResponses;
-
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
-use Symfony\Component\HttpFoundation\Response;
 
 class StudentController extends Controller
 {
     use HttpResponses;
 
-    public function index()
+    public function index(Request $request)
     {
         try {
+            $user = $request->user();
 
-            $students = Student::all();
-            return $students;
+            if (!$user->can('get-students')) {
+                return response()->json(['error' => 'Não autorizado'], 403);
+            }
 
-        } catch (\Exception $exception) {
-            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+            $name = $request->input('name');
+            $cpf = $request->input('cpf');
+            $email = $request->input('email');
+
+            $studentsQuery = Student::query();
+
+            $studentsQuery->where('user_id', $user->id);
+
+            if ($name) {
+                $studentsQuery->where('name', 'like', "%$name%");
+            }
+
+            if ($cpf) {
+                $studentsQuery->orWhere('cpf', 'like', "%$cpf%");
+            }
+
+            if ($email) {
+                $studentsQuery->orWhere('email', 'like', "%$email%");
+            }
+
+            $students = $studentsQuery->orderBy('name')->get();
+
+            $formattedStudents = $students->map(function ($student) {
+                return [
+                    'id' => $student->id,
+                    'name' => $student->name,
+                    'email' => $student->email,
+                    'contact' => $student->contact,
+                    'cpf' => $student->cpf,
+                ];
+            });
+
+            return response()->json($formattedStudents, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Erro interno do servidor'], 500);
         }
     }
 
